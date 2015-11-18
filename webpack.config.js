345266ee4f3469c0439cbb7bccbb78135e6de3a2
Adam Simpson <adam@heysparkbox.com>
2015-11-18 12:23:22 -0500

    chore: add cache to webpack babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,10 @@ module.exports = {
         test: /\.js$/, 
         exclude: /node_modules/,
         loader: 'babel',
+
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          cacheDirectory: true
         }
       }
     ]
