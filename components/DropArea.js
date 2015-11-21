bd4f5dcf9a8295646300c98532993605c41b812f
philip@phanza.com <philip@phanza.com>
2015-11-21 15:50:35 -0500

    Updates styles

--- components/DropArea.js
+++ components/DropArea.js
@@ -6,7 +6,7 @@ const DropArea = (props) => {
       <div className="drop-content">
         <div className="drop-box">
           <svg viewBox="0 0 34 46">
-            <g className="arrow" fill="none" transform="translate(1, 1)" stroke="#FFA800" strokeWidth="2">
+            <g className="arrow" transform="translate(1, 1)">
               <path d="M16,0 L16,42" strokeLinecap="square" />
               <path d="M0,26 L15.9023353,43 L32,26" />
             </g>
@@ -14,13 +14,13 @@ const DropArea = (props) => {
         </div>
         <div className="drop-message">
           <div className="start-message">
-            Drag your project folder to the window.
+            Drag your project folder to the window
           </div>
           <div className="release-message">
             Now let go over the folder
           </div>
           <div className="loading-message">
-            Loading the project.
+            Loading the project
           </div>
         </div>
       </div>
