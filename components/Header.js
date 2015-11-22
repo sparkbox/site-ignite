01d08c0eb996aa042e111c331cd38bdde8bff7dd
philip@phanza.com <philip@phanza.com>
2015-11-22 13:50:14 -0500

    Add running view animation

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,14 @@ import React from "react";
 const Header = (props) => {
   return (
       <span>
-        <header>
-          <h1>{props.data.title}</h1>
-          <h2>version <span className="highlight">{props.data.version}</span></h2>
-        </header>
-        <div className="url">
-        {props.data.ip}<span className="highlight">{props.data.port}</span>
+        <div className="running">
+          <header>
+            <h1>{props.data.title}</h1>
+            <h2>version <span className="highlight">{props.data.version}</span></h2>
+          </header>
+          <div className="url">
+          {props.data.ip}<span className="highlight">{props.data.port}</span>
+          </div>
         </div>
       </span>
   );
